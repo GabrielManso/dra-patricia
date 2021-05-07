2427e0a2a8ebc9844f98ea3b69fe2b287e7a23f0
Gabriel Manso <glopesmanso@gmail.com>
2021-05-06 22:59:19 -0300

    Final-08

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -17,30 +17,32 @@ const Contacts = () => {
   return (
     <div>
       <Header />
-      <div className="contato-geral">
-        <h1>Contato</h1>
-        <div className="formas-contato">
-          <p>clique no botão abaixo e fale conosco</p>
-          <button onClick={whatsapp} className="whats">Whatsapp</button>
-          <p>clique no botão abaixo e nos envie um e-mail</p>
-          <button className="email">E-mail</button>
-          <div className="redes-sociais">
-            <h2>Redes Sociais</h2>
-            <ul>
-              <li>
-              <button onClick={facebook}> <img src="/Img/003-facebook.png" alt="Logo Facebook"/></button>      
-              </li>
-              <li>
-              <button onClick={youtube}> <img src="/Img/001-youtube.png" alt="Logo Youtube"/></button>
-              </li><li>
-              <button onClick={instagram}> <img src="/Img/002-instagram.png" alt="Logo Intgram"/></button>
-              </li><li>
-              <button onClick={linkedin}> <img src="/Img/004-linkedin.png" alt="Logo Linkedin"/></button>
-              </li>
-            </ul>
+      <GridConteiner>
+        <div className="contato-geral">
+          <h1>Contato</h1>
+          <div className="formas-contato">
+            <p>clique no botão abaixo e fale conosco</p>
+            <button onClick={whatsapp} className="whats">Whatsapp</button>
+            <p>clique no botão abaixo e nos envie um e-mail</p>
+            <button className="email">E-mail</button>
+            <div className="redes-sociais">
+              <h2>Redes Sociais</h2>
+              <ul>
+                <li>
+                <button onClick={facebook}> <img src="/Img/003-facebook.png" alt="Logo Facebook"/></button>      
+                </li>
+                <li>
+                <button onClick={youtube}> <img src="/Img/001-youtube.png" alt="Logo Youtube"/></button>
+                </li><li>
+                <button onClick={instagram}> <img src="/Img/002-instagram.png" alt="Logo Intgram"/></button>
+                </li><li>
+                <button onClick={linkedin}> <img src="/Img/004-linkedin.png" alt="Logo Linkedin"/></button>
+                </li>
+              </ul>
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
+      </GridConteiner>
     </div>
   )
 }
